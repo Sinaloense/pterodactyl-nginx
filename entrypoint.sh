88d0bb1e7804cd18107c3583e1d04645cbe4027e
Sinaloense <sinaloensemel@gmail.com>
2025-12-05 17:08:37 -0700

    Update install script, PHP changes apply only when using Nginx + PHP-FPM

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,15 +4,6 @@ cd /home/container
 # Output Current Nginx Version
 nginx -v
 
-# Create Nginx logs directory
-mkdir -p /home/container/nginx/logs
-
-# Create Nginx tmp directory
-mkdir -p /home/container/nginx/tmp
-
-# Create html directory
-mkdir -p /home/container/html
-
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
